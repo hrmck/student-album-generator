81df0dc6327a4cd1d501c7a7d1a37b0c7b2878d8
Agnes Leung <dev@agnesleung.me>
2023-02-21 16:13:13 +0900

    Merge branch 'fix-left-page'

--- student_album_generator/config.py
+++ student_album_generator/config.py
@@ -2,7 +2,12 @@ from pathlib import Path
 from typing import Final
 
 SCHOOL_NAME = "Holy Trinity College"
+HTML_FOLDER = "html-report"
 IMAGE_FOLDER = "img"
 CSS_FOLDER = "css"
-abs_image_folder: Final = f"file:///{Path.cwd()}/{IMAGE_FOLDER}"
-abs_css_folder: Final = f"file:///{Path.cwd()}/{CSS_FOLDER}"
+
+abs_image_folder: Final = f"{Path.cwd()}/{IMAGE_FOLDER}"
+abs_css_folder: Final = f"{Path.cwd()}/{CSS_FOLDER}"
+# specific format for selenium to link images to pdf
+export_image_folder: Final = f"file:///{Path.cwd()}/{IMAGE_FOLDER}"
+export_css_folder: Final = f"file:///{Path.cwd()}/{CSS_FOLDER}"
--- student_album_generator/utils.py
+++ student_album_generator/utils.py
@@ -1,8 +1,8 @@
-from config import IMAGE_FOLDER, abs_image_folder, CSS_FOLDER, abs_css_folder
+from config import IMAGE_FOLDER, export_image_folder, CSS_FOLDER, export_css_folder
 
 
 def get_css_path(use_relative_path: bool = False) -> str:
-    return "/" + CSS_FOLDER if use_relative_path else abs_css_folder
+    return "/" + CSS_FOLDER if use_relative_path else export_css_folder
 
 
 def display_academic_year(year: str) -> str:
